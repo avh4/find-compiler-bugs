7749d40d30e3bd002e75d6832594ce551308e1bf
Aaron VonderHaar <av@noredink.com>
2017-07-04 19:00:24 -0700

    Add /reset and /writeElmFile

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const cors = require("cors");
+const child_process = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
 // Constants
 const PORT = 8080;
@@ -15,9 +18,86 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
 
 app.post("/compile", function(req, res) {
-  console.log("compile", req.body.filename);
+  const filename = req.body.filename;
+  console.log("compile", filename);
 
-  res.send("{}");
+  child_process.execFile(
+    "elm-make",
+    ["--yes", filename],
+    { cwd: "work" },
+    function(err, stdout, stderr) {
+      if (err) {
+        console.log("Err", stderr);
+        res.send({
+          code: err.code,
+          stdout: stdout,
+          stderr: stderr
+        });
+      } else {
+        console.log("OK");
+        res.send({
+          code: 0,
+          stdout: stdout,
+          stderr: stderr
+        });
+      }
+    }
+  );
+});
+
+app.post("/writeElmFile", function(req, res) {
+  const filename = req.body.filename;
+  console.log("writeElmFile", filename);
+
+  fs.writeFile(path.join("work", filename), "empty", function(err) {
+    if (err) {
+      console.log("Err", err);
+      res.send({
+        code: 1,
+        stdout: "",
+        stderr: err.toString()
+      });
+    } else {
+      console.log("OK");
+      res.send({
+        code: 0,
+        stdout: "",
+        stderr: ""
+      });
+    }
+  });
+});
+
+app.post("/reset", function(req, res) {
+  console.log("reset");
+
+  child_process.execFile("rm", ["-rf", "work"], { cwd: "." }, function(
+    err,
+    stdout1,
+    stderr1
+  ) {
+    child_process.execFile("mkdir", ["-p", "work"], { cwd: "." }, function(
+      err,
+      stdout2,
+      stderr2
+    ) {
+      if (err) {
+        console.log("Err", stderr);
+        res.send({
+          code: err.code,
+          stdout: stdout1 + "\n" + stdout2,
+          stderr: stderr1 + "\n" + stderr2
+        });
+      } else {
+        console.log("OK");
+        res.send({
+          code: 0,
+          stdout: stdout1 + "\n" + stdout2,
+          stderr: stderr1 + "\n" + stderr2
+        });
+      }
+    });
+  });
 });
 
 app.listen(PORT, HOST);
